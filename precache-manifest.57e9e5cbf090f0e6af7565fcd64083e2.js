60027a87e99ee47899f76017bc424cb2dc116862
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-10 13:50:35 +0000

    Deploy xlfsummer/beat-saber-version-transformer to github.com/xlfsummer/beat-saber-version-transformer.git:gh-pages

---
+++ precache-manifest.57e9e5cbf090f0e6af7565fcd64083e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7df0ac6b8638de82b005",
+    "url": "/css/app.7a3f8659.css"
+  },
+  {
+    "revision": "7df0ac6b8638de82b005",
+    "url": "/js/app.b0598e4c.js"
+  },
+  {
+    "revision": "04e580cc0ae2bbd90cde",
+    "url": "/js/chunk-vendors.ed7f2d64.js"
+  },
+  {
+    "revision": "b07154c1dcb74cc7cb1d923cf3c3e4d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
